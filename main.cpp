b1142ce344fc9fef64f4a94a22dc53a474b74dd5
quynh <pnquynh0210@gmail.com>
2024-03-30 10:19:47 +0700

    hide cursor

--- main.cpp
+++ main.cpp
@@ -11,14 +11,15 @@ int main() {
   // Default config
   GameConfig config = {4, 5, 4, {2, 4, 6, 8}};
   GameManager game = GameManager(config);
-
+  setCursor(false);
   while (true) {
-    system("cls");
     game.displayBoard();
     VECI dir = getConsoleInput();
     if (dir.first == -2 && dir.second == -2)
       game.pickCell();
     else
       game.moveCursor(dir); 
+
+    Sleep(1000/60);
   }
 }
